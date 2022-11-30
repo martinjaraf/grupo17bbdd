f7645b8c97522b066ff6b1ee5fa67a5d8b89350e
Martin Rosenfeld <martn.rosenfeld@uc.cl>
2022-11-30 19:21:37 -0300

    cambios Martin R

--- sesion.php
+++ sesion.php
@@ -48,9 +48,44 @@
                         <a class="btn btn-secondary mb-2" href="consultas.php">Importar usuarios
                         </a>
                         <br>
-                        <a class="btn btn-secondary mb-2" href="sesion2.php">Inicio de Sesión
+
+                        <form align="center" action="consultas/consulta_tipo_nombre.php" method="post">
+                        Nombre:
+                        <input type="text" name="nombre_usuario">
+                        <br/>
+                        Contrasena:
+                        <input type="text" name="contrasena_usuario">
+                        <br/><br/>
+                        <input type="submit" value="Buscar">
+                        </form>
+
+                        <br>
+                        <a class="btn btn-secondary mb-2" href="sesion.php">Iniciar Sesión
                         </a>
                     </div>
 
                     <script async type="text/javascript" src="../js/bulma.js"></script>
-    </body>
+        
+    <?php
+    #Primero obtenemos todos los tipos de pokemones
+    require("config/conexion.php");
+    $result = $db -> prepare("SELECT DISTINCT nombre FROM usuarios;");
+    $result -> execute();
+    $dataCollected = $result -> fetchAll();
+    ?>
+
+    <form align="center" action="consultas/consulta_tipo.php" method="post">
+      Seleccinar un tipo:
+      <select name="nombre">
+        <?php
+        #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
+        foreach ($dataCollected as $d) {
+          echo "<option value=$d[0]>$d[0]</option>";
+        }
+        ?>
+      </select>
+      <br><br>
+      <input type="submit" value="Verificar usuario">
+    </form>
+
+</body>
